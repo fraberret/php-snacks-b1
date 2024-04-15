a1f52c87eff1bdf3cf06dc25b510d13bb2d1e5fe
fraberret <f.berretta02@gmail.com>
2024-04-15 12:51:55 +0200

    create form

---
+++ snack-2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+<form action="" method="GET">
+
+    <label class="form-label" for="name">Name</label>
+    <input class="form-control" type="text" name="name" id="">
+
+    <label class="form-label" for="mail">Mail</label>
+    <input class="form-control" type="text" name="mail" id="">
+
+    <label class="form-label" for="mail">Age</label>
+    <input class="form-control" type="text" name="age" id="">
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+
+</form>
+</div>
+
+</body>
+</html>
