0b5f58d6b03e9f185214d23cc6a04394882f3104
fraberret <f.berretta02@gmail.com>
2024-04-15 11:57:16 +0200

    Initial Commit and create an array

---
+++ index.php
@@ -0,0 +1,15 @@
+
+
+
+<?php
+
+$partite_di_basket = array(
+    array("casa" => "Squadra A", "ospite" => "Squadra B", "punti_casa" => 98, "punti_ospite" => 102),
+    array("casa" => "Squadra C", "ospite" => "Squadra D", "punti_casa" => 89, "punti_ospite" => 95),
+    array("casa" => "Squadra E", "ospite" => "Squadra F", "punti_casa" => 110, "punti_ospite" => 105)
+    
+);
+
+var_dump($partite_di_basket)
+
+?>
